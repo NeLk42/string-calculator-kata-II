c0169e0f3dec107a52cca5f779de1ccce9bb9113
NeLk <nelk42@gmail.com>
2014-10-13 16:49:32 +0100

    should use custom delims refactor

--- src/calc.js
+++ src/calc.js
@@ -2,26 +2,20 @@ var calc = {
     add: function(text) {
         var result = text ? text : 0;
         if (result != 0){
-            var delimIndex = text.indexOf('//')
-            if(delimIndex >= 0) {
-                var numbers = this.getNumsDelim(text)
-                result = this.getSum(numbers)
-            }
-            else {
-                var numbers = this.getNumbers(text)
-                result = this.getSum(numbers)
-            }
+            var delim = text.indexOf('//')
+            var numbers = this.getNumbers(text, delim)
+            result = this.getSum(numbers)
         }
         return result
     },
-    getNumbers: function(text) {
-        var res = text.replace('\n',',').split(',')
-        return res
-    },
-    getNumsDelim: function (text) {
-        var subtext = text.substring(2)
-        var textArray = subtext.split('\n')
-        var res = textArray[1].split(textArray[0])
+    getNumbers: function(text,delim) {
+        if(delim < 0){
+            var res = text.replace('\n',',').split(',')
+        } else {
+            var subtext = text.substring(2)
+            var textArray = subtext.split('\n')
+            var res = textArray[1].split(textArray[0])
+        }
         return res
     },
     getSum: function(array){
